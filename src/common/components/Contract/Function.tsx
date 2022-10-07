1697b6f6e9772d61764c8310799c895abc2bd1f6
nickadamson <naburner@protonmail.com>
2022-10-07 14:09:52 -0700

    refactor: formatting of inputs

--- src/common/components/Contract/Function.tsx
+++ src/common/components/Contract/Function.tsx
@@ -30,30 +30,7 @@ const Function = ({
   const { address } = useAccount();
   // useState for all input values
   const [formData, setFormData] = useState(
-    inputs.map((input) => {
-      let value;
-      switch (input.type) {
-        case "address":
-          value = address ?? "0x0000000000000000000000000000000000000000";
-          break;
-        case "string":
-          value = "";
-          break;
-        case "bool":
-          value = false;
-          break;
-
-        default:
-          if (input.type.includes("int")) value = 1;
-          else if (input.type.includes("fixed")) value = 1.1;
-          else if (input.type.includes("bytes"))
-            value = "0x0000000000000000000000000000000000000000";
-          else console.log(`Unknown Type: ${JSON.stringify(input)}`);
-          break;
-      }
-
-      return { value, ...input };
-    })
+    formatIODefaultValues(inputs, address) // .sol types -> .js types
   );
   const [txWillError, setTxWillError] = useState(true);
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,39 @@
+import { JsonFragmentType } from "@ethersproject/abi";
+
 export function toTrimmedAddress(value: string) {
   if (!value) return "";
   return (
     value.substr(0, 6) + "…" + value.substr(value.length - 4, value.length)
   );
+}
+
+export function formatIODefaultValues(
+  inputs: readonly JsonFragmentType[],
+  address: string
+) {
+  return inputs.map((input) => {
+    let value;
+
+    switch (input.type) {
+      case "address":
+        value = address ?? "0x0000000000000000000000000000000000000000";
+        break;
+      case "string":
+        value = "";
+        break;
+      case "bool":
+        value = false;
+        break;
+
+      default:
+        if (input.type.includes("int")) value = 1;
+        else if (input.type.includes("fixed")) value = 1.1;
+        else if (input.type.includes("bytes"))
+          value = "0x0000000000000000000000000000000000000000";
+        else console.log(`Unknown Type: ${JSON.stringify(input)}`);
+        break;
+    }
+
+    return { value, ...input };
+  });
 }
